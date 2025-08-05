858c1c240a0c038b10d5e61bb8d67253bfa6d230
GormWH <sp4kid@gmail.com>
2025-08-06 02:55:03 +0900

    Feat Load Data at Start-up

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.MusicStore.Messages;
+using Avalonia.MusicStore.Models;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 
@@ -9,6 +11,11 @@ namespace Avalonia.MusicStore.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+    
+    public MainWindowViewModel()
+    {
+        LoadAlbums();
+    }
     
     [RelayCommand]
     private async Task AddAlbumAsync()
@@ -20,4 +27,16 @@ public partial class MainWindowViewModel : ViewModelBase
             await album.SaveToDiskAsync(); // Add this line
         }
     }
+    
+    private async void LoadAlbums()
+    {
+        var albums = (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x)).ToList();
+        foreach (var album in albums)
+        {
+            Albums.Add(album);
+        }
+        var coverTasks = albums.Select(album => album.LoadCover());
+        await Task.WhenAll(coverTasks);
+    }
+
 }
