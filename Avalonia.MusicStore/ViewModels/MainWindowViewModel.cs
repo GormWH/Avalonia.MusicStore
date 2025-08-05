c31126e56794de0a51f3b07b7650ebe6c9d8f16a
GormWH <sp4kid@gmail.com>
2025-08-06 00:44:02 +0900

    Feat Add Items to the User's Collection

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Avalonia.MusicStore.Messages;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
@@ -7,9 +8,15 @@ namespace Avalonia.MusicStore.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+    
     [RelayCommand]
     private async Task AddAlbumAsync()
     {
         var album = await WeakReferenceMessenger.Default.Send(new PurchaseAlbumMessage());
+        if (album is not null)
+        {
+            Albums.Add(album);
+        }
     }
 }
